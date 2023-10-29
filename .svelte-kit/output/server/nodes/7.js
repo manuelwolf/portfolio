636d0d66587d826043f8be2fcea05e2dd3b164ac
Manuel Wolf <manuellwolf@gmail.com>
2023-10-29 18:22:04 +0100

    Set name and add skills

--- .svelte-kit/output/server/nodes/7.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 7;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/resume/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/7.38b75382.js","_app/immutable/chunks/scheduler.bdaa4df1.js","_app/immutable/chunks/index.49b0a900.js","_app/immutable/chunks/Chip.0696ab9f.js","_app/immutable/chunks/TabTitle.9672f816.js","_app/immutable/chunks/params.352303bf.js","_app/immutable/chunks/paths.1dcded33.js","_app/immutable/chunks/CommonPage.a4b6cc3c.js","_app/immutable/chunks/MainTitle.de67ff74.js"];
-export const stylesheets = ["_app/immutable/assets/7.c6b04c59.css"];
-export const fonts = [];
